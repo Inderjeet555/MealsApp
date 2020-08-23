80a53ffac2a1c7706a1e3c6c6cba42e303deb63d
Inderjeet Singh <meet2indersingh@gmail.com>
2020-08-23 22:05:16 +0530

    InitialCommit

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(title: Text('DeliMeals'),),
+          body: Center(
+            child: Text('Details Of recipe'),
+      ),
+    );
+  }
+}
